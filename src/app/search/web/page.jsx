f723be1f6ce6455ca53be994009f6458b3f1a8f9
Anil <ak26122k@gmail.com>
2023-10-04 01:05:10 +0530

    fetch data from google programmable search engine api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+export default async function page({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
 
-export default function page() {
-  return <div>page</div>;
+  const results = data.items;
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
