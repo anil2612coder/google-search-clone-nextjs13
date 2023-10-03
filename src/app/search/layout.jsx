1bf0249528d7090cb3c4f92568c21207dcbf1205
Anil <ak26122k@gmail.com>
2023-10-03 16:10:57 +0530

    make image and web page and layout

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,14 @@
+import SearchHeader from "@/components/SearchHeader";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function SearchLayout({ children }) {
+  return (
+    <div>
+      <SearchHeader />
+      {children}
+    </div>
+  );
+}
