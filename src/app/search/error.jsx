ecf6255479f1ba1a1ec540067daa1a3ca9f2af20
Anil <ak26122k@gmail.com>
2023-10-04 16:11:14 +0530

    some error

--- src/app/search/error.jsx
+++ src/app/search/error.jsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function error({ error, reset }) {
-  useEffect(() => {}, [error]);
   return (
     <div className="flex flex-col justify-center items-center pt-10 ">
       <h1 className="text-3xl mb-4">Something went wrong</h1>
